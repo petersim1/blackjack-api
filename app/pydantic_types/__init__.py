8b397fcb8ab69e869dedfd683a19333d62f03be7
Peter Simone <plssimone@gmail.com>
2023-11-19 19:26:11 -0500

    create new response pydantic type

--- app/pydantic_types/__init__.py
+++ app/pydantic_types/__init__.py
@@ -1,6 +1,5 @@
 from typing import List, Dict, Optional, Union, Tuple
-from pydantic import BaseModel
-from app.modules.cards import Card
+from pydantic import BaseModel, Field
 
 class StateActionPair(BaseModel):
     player_show: int
@@ -27,17 +26,53 @@ class RulesI(BaseModel):
     allow_surrender: bool=True
 
 class MessageSend(BaseModel):
-    cards_remaining: float
-    round_over: bool
-    deal_house: bool
-    deal_player: bool
-    profit: float
-    count: Tuple[int, float]
-    text: str
-    player_total: int
-    house_cards: List[Tuple[Union[str, int], str]] = []
-    player_cards: List[Tuple[Union[str, int], str]] = []
-    policy: List[str] = []
+    ready: bool = Field(
+        default=True,
+        description="whether game was initialized and can be dealt"
+    )
+    round_over: bool = Field(
+        default=False,
+        description="whether the round is over"
+    )
+    hand_result: Tuple[str, int] = Field(
+        description="result of the hand (text, profit)"
+    )
+    round_profit: float = Field(
+        default=0,
+        description="result of the round (all hands)"
+    )
+    total_profit: float = Field(
+        default=0.0,
+        description="running profit per session"
+    )
+    count: Tuple[int, float] = Field(
+        default_factory=lambda : [0, 0.0],
+        description="[running count, true count]"
+    )
+    cards_remaining: float = Field(
+        default=1.0,
+        description="percent of cards remaining in shoe"
+    )
+    player_total: int = Field(
+        default=0,
+        description="total of player's current hand"
+    )
+    player_cards: List[Tuple[Union[str, int], str]] = Field(
+        default_factory=lambda : [],
+        description="list of cards for player"
+    )
+    house_total: int = Field(
+        default=0,
+        description="total of house's current hand"
+    )
+    house_cards: List[Tuple[Union[str, int], str]] = Field(
+        default_factory=lambda : [],
+        description="list of cards for house"
+    )
+    policy: List[str] = Field(
+        default_factory=lambda : [],
+        description="current policy for player"
+    )
 
 QMovesI = Dict[str, float]
 
