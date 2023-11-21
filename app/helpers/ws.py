d74c1f222527d581cfe47c704bc46f793d0c71fe
Peter Simone <plssimone@gmail.com>
2023-11-20 20:25:11 -0500

    rework flow of messages on initial deal

--- app/helpers/ws.py
+++ app/helpers/ws.py
@@ -6,6 +6,8 @@ if TYPE_CHECKING:
     from app.routes.ws import Consumer
 
 def handle_init(data: dict):
+    """ will throw an error, which we catch in the fct that calls this, if pydantic validation error is thrown."""
+
     rules = data.get("rules", RulesI().model_dump())
     deck = data.get("deck", DeckI().model_dump())
     rules = RulesI(**rules)
@@ -13,6 +15,7 @@ def handle_init(data: dict):
     game_hyperparams = deck.model_dump()
     game_hyperparams["rules"] = rules.model_dump()
     return game_hyperparams
+
 
 def get_game_info(obj: "Consumer") -> Tuple[
     List[CardDesctructured],
@@ -26,7 +29,7 @@ def get_game_info(obj: "Consumer") -> Tuple[
     player_cards = [[card.card,card.suit] for card in obj.game.players[0].cards[0].cards]
     c_remaining = len(obj.game.shoe.cards) / (52 * obj.game.n_decks)
     house_total = None
-    if obj.game.house_played or obj.game.house_blackjack:
+    if obj.game.house_played:
         house_total = obj.game.house.cards[0].total
     player_total = obj.game.players[0].cards[0].total
     policy = obj.game.players[0].get_valid_moves()
@@ -53,20 +56,8 @@ def gather_responses(obj: "Consumer", data: dict, code: str) -> Iterator[Message
             obj.game.step_player(0, move)
 
     h_c, p_c, c_rem, h_t, p_t, policy = get_game_info(obj)
+    h_c[-1] = ["Hidden", "Hidden"] # always hide here, following logic excludes this.
 
-    if not obj.game.house_played:
-        if not obj.game.house_blackjack:
-            # hide the card from the client, but still present a "card" in the array.
-            h_c[-1] = ["Hidden", "Hidden"]
-        else:
-            hand_result = ("House Blackjack :(", -wager)
-            policy = []
-            round_over = True
-            if p_t == 21:
-                hand_result = ("push", 0)
-            else:
-                obj.balance -= wager
-    
     yield MessageSend(
         cards_remaining=c_rem,
         round_over=round_over,
